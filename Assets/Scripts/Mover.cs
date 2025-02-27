b85afa3c52fdae9a8d530dcf77a0174597a4bc2d
Chloe <duyenpham.june.1995@gmail.com>
2025-02-27 11:09:57 +1100

    First commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    float xValue = 0f;
+    float yValue = 0.002f;
+    float zValue = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
