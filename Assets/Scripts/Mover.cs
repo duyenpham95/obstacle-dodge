08bba6672813df8e71cd829545a959902f254035
Chloe <duyenpham.june.1995@gmail.com>
2025-02-27 11:39:28 +1100

    Input GetAxis to move object

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,19 +3,18 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.002f;
-    [SerializeField] float zValue = 0f;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float xValue = Input.GetAxis("Horizontal"); // moving left and right 
+        float yValue = 0f;  // moving up and down
+        float zValue = Input.GetAxis("Vertical"); // moving to the screen
         transform.Translate(xValue, yValue, zValue);
     }
 }
