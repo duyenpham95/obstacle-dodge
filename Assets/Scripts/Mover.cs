793adce6b302d460eef32e97e6ad1cb8bfb90f4a
Chloe <duyenpham.june.1995@gmail.com>
2025-02-27 14:35:55 +1100

    move to functions

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,10 +7,23 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstruction() 
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using arrow keys or wasd");
+        Debug.Log("Welcome to the game!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; // moving left and right 
         float yValue = 0f;  // moving up and down
