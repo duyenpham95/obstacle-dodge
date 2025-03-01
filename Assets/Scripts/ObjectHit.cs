722aa4f1e867d8f85a054809f4567ee7c627585f
duyenpham95 <duyenpham.june.1995@gmail.com>
2025-03-01 15:13:45 +1100

    Using Tag

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+
         Debug.Log("Somthing hit me");
     }
 }
